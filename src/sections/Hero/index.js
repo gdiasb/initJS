edf0ae469eea5d0ddb45516a28b00b056a6cf76b
gabriela <gdias@peq.coppe.ufrj.br>
2022-03-15 23:24:35 -0300

    :construction: Create Main() section content
    Added principal text and avatar image. It is necessary to improve responsiveness.

--- src/sections/Hero/index.js
+++ src/sections/Hero/index.js
@@ -1,11 +1,18 @@
-import './style.css'
+import "./style.css";
+
 
 function Hero() {
   return `
   <section class="hero">
-    <p>Uai</p>
+    <div class='content-box-text'>
+      <h1>Hi, I'm Gabriela! An enthusiast front-end developer and former chemical engineer.</h1>
+      <h2>Always learning something new, and, currently, is HTML, CSS, JS, and React.</h2>
+      </div>
+      <div class="content-box-avatar">
+      <img src='https://i.im.ge/2022/03/16/lEXktr.png' />    
+    </div>
   </section>
-  `
+  `;
 }
 
 export default Hero;
